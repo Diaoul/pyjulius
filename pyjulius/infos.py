3e6897155a5c2841bb37f71449b61efb6a895f15
Antoine Bertin <diaoulael@gmail.com>
2012-01-13 23:29:44 +0100

    Release v0.2

--- pyjulius/infos.py
+++ pyjulius/infos.py
@@ -15,4 +15,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with pyjulius.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '0.1'
+__version__ = '0.2'
